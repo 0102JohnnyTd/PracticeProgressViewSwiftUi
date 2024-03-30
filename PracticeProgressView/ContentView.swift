d42fbac8d547f3d652c6d716e8d8b13c3c9e0deb
0102johnny <rt2824.recruit.1022@gmail.com>
2024-03-30 12:48:32 +0900

    Initial commit

---
+++ PracticeProgressView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeProgressView
+//
+//  Created by Johnny Toda on 2024/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeProgressView/PracticeProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProgressViewApp.swift
+//  PracticeProgressView
+//
+//  Created by Johnny Toda on 2024/03/30.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
